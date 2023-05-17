0511ab6753ae18a04eab8ebb6e66202b0337b8a9
G0od1k <g0cd1k@gmail.com>
2023-05-18 00:59:22 +0200

    Added time into the video controls

--- client/js/video.js
+++ client/js/video.js
@@ -12,3 +12,26 @@ video.onpause = () => {
 playButton.onclick = () => {
     video.paused ? video.play() : video.pause()
 }
+
+video.ontimeupdate = function () {
+    let hours = video.duration >= 3600
+
+    function formatTime(seconds) {
+        return new Date((seconds || 0) * 1000 + 82800000).toLocaleTimeString(
+            undefined,
+            {
+                hour: hours ? "2-digit" : undefined,
+                minute: "2-digit",
+                second: "2-digit",
+                hourCycle: "h23",
+            }
+        )
+    }
+
+    document.querySelector("#time").innerText = [
+        video.currentTime,
+        video.duration,
+    ]
+        .map((x) => formatTime(x))
+        .join("\n")
+}
