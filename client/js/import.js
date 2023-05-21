8a9764eeb48f62e952279199df5eba6f003e314f
G0od1k <g0cd1k@gmail.com>
2023-05-21 19:39:33 +0200

    Added import playlist button

---
+++ client/js/import.js
@@ -0,0 +1,13 @@
+document.querySelector(`#import`).onchange = function () {
+    const fr = new FileReader()
+
+    fr.onload = function () {
+        const data = JSON.parse(fr.result)
+        data.forEach((x) => {
+            socket.emit("add", x.name, x.url, x.audio, x.vtt)
+        })
+    }
+
+    fr.readAsText(this.files[0])
+    this.value = null
+}
