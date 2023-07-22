86dc143ef77d4d9b753c011b5bf953357c9090a9
G0od1k <g0cd1k@gmail.com>
2023-07-22 22:20:12 +0200

    Added room entry by URL

--- client/js/room.js
+++ client/js/room.js
@@ -4,3 +4,14 @@ document.querySelector(`#room`).addEventListener("submit", (e) => {
     e.preventDefault()
     socket.emit("join", e.target.querySelector("input").value.toUpperCase())
 })
+
+socket.emit(
+    "join",
+    new URLSearchParams(window.location.search).get("room")?.toUpperCase()
+)
+
+document.querySelector("#room > footer > .copy").onclick = () => {
+    navigator.clipboard.writeText(
+        new URL("?room=" + room.code, window.location.href).href
+    )
+}
